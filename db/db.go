da76340810ad217cdef47d3296ed62786c2d1515
Takdirul Islam Shishir <39631235+iamtakdir@users.noreply.github.com>
2022-03-21 00:04:55 +0600

    dd

--- db/db.go
+++ db/db.go
@@ -2,17 +2,31 @@ package db
 
 import (
 	"github.com/iamtakdir/url-shortner-go/models"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	// "gorm.io/driver/postgres"
 	"log"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
+// func Connect() {
+// 	//changed database config
+// 	dsn := "host=localhost user=postgres password=843543 dbname=testdb port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+// 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+// 	if err != nil {
+// 		log.Fatalln(err)
+// 	}
+// 	log.Println("Database Connected Successfully")
+// 	DB = db
+// 	db.AutoMigrate(&models.ShortUrlModel{})
+// }
+
 func Connect() {
 	//changed database config
 	dsn := "host=localhost user=postgres password=843543 dbname=testdb port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatalln(err)
 	}
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/google/uuid v1.3.0
 	github.com/labstack/echo/v4 v4.7.0
 	gorm.io/driver/postgres v1.3.1
+	gorm.io/driver/sqlite v1.3.1
 	gorm.io/gorm v1.23.2
 )
 
